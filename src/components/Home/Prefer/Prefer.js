51f2297957873141f84ff7a22e8306210ba5095c
DigitalAgencySolutionsINC <marketing@digitalagencysolutionsinc.com>
2024-01-15 23:26:07 +0800

    update

--- src/components/Home/Prefer/Prefer.js
+++ src/components/Home/Prefer/Prefer.js
@@ -1,26 +1,60 @@
-import image from '@/constant/Images/image'
-import Image from 'next/image'
+import image from '@/constant/Images/image';
+import Image from 'next/image';
 
 const Prefer = () => {
   return (
     <section className='py-24 sm:py-32 bg-slate-50'>
-    <div className='app__container grid grid-cols-1 lg:grid-cols-2 gap-8'>
-    <div className='mx-auto'>
-    <Image src={image.bannerMan} alt='digital marketing'/>
-    </div>
-    <div>
-    <h1 className='primary-heading'>Do you remember the story of <span className='highlight'>the rabbit and turtle?</span></h1>
-    <p className='pt-12 pb-6 app__text'>Kids believe that the turtle was hardworking, but the truth is that if the rabbit had not  been distracted, the turtle would have lost the race. You alone, looking after investment and creativity along with your business, will make you the distracted rabbit. WebDev Private Limited comes into the story now, follows the rabbit through the race, and then reminds him of his self-worth and power. The rabbit stays the fastest and smartest in the jungle!</p>
-    <ul className='list-inside list-disc app__text'>
-    <li>We have a team of graphic designers who create the best and most suitable templates and promotional videos for you. These won’t just attract customers; they will also make them pay a visit and buy something. Looks and presentation are our jobs.</li>
-    <li>We prepare the best for you so that when you represent yourself, you feel proud of who you are and have always been, and the same confidence may be seen by the customers.</li>
-    <li>Our graphic design experts use tools like Canva, Adobe Premium, and Lightroom Premium. All that is hot in the market is what we provide.</li>
-    </ul>
-    </div>
-
-    </div>
+      <div className='app__container grid grid-cols-1 lg:grid-cols-2 gap-8'>
+        <div className='mx-auto'>
+          <Image src={image.bannerMan} alt='digital marketing' />
+        </div>
+        <div>
+          <h1 className='primary-heading'>
+            Why Choose DAS for{' '}
+            <span className='highlight'>Digital Marketing Services?</span>
+          </h1>
+          <p className='pt-12 pb-6 app__text'>
+            Welcome to DAS, your trusted partner for Digital Marketing Services.
+            Our team comprises skilled professionals with a solid track record
+            in digital marketing and web development. What sets us apart is our
+            commitment to tailoring solutions that align with your specific
+            business goals. We prioritize your success through a client-centric
+            approach, keeping you engaged and informed throughout the process.
+            At DAS, we thrive on innovation, utilizing the latest technologies
+            to keep you ahead in the digital world. Our primary focus is on
+            delivering measurable results to boost your online presence and
+            drive business growth. Choose DAS for a partnership dedicated to
+            taking your digital presence to the next level.
+          </p>
+          <ul className='list-inside list-disc app__text'>
+            <li>
+              Expertise: Our team consists of seasoned professionals with a
+              proven track record in digital marketing and web development.
+            </li>
+            <li>
+              Custom Solutions: We understand that every business is unique. Our
+              services are tailored to meet your specific goals and
+              requirements.
+            </li>
+            <li>
+              Innovation: Stay ahead in the digital landscape with our
+              innovative solutions and cutting-edge technologies.
+            </li>
+            <li>
+              Client-Centric Approach: Your success is our priority. We work
+              closely with you, keeping you informed and involved throughout the
+              process.
+            </li>
+            <li>
+              Results-Driven: We are committed to delivering measurable results.
+              Our strategies are designed to enhance your online presence and
+              drive business growth.
+            </li>
+          </ul>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Prefer
+export default Prefer;
--- src/components/Home/WeAre/WeAre.js
+++ src/components/Home/WeAre/WeAre.js
@@ -1,38 +1,49 @@
-import image from "@/constant/Images/image";
-import Image from "next/image";
-import "./WeAre.css";
-import Link from "next/link";
+import image from '@/constant/Images/image';
+import Image from 'next/image';
+import './WeAre.css';
+import Link from 'next/link';
 
 const WeAre = () => {
   return (
-    <section className="we-are-area flex items-center bg-slate-50 py-24 md:py-32">
-      <div className="app__container grid grid-cols-1 md:grid-cols-2 gap-8 relative">
-        <div className="we-are-img">
-          <div className="we-are-banner-img">
-            <Image src={image.banner} alt="" />
+    <section className='we-are-area flex items-center bg-slate-50 py-24 md:py-32'>
+      <div className='app__container grid grid-cols-1 md:grid-cols-2 gap-8 relative'>
+        <div className='we-are-img'>
+          <div className='we-are-banner-img'>
+            <Image src={image.banner} alt='' />
           </div>
-          <div className="we-are-animation">
-            <Image src={image.like} alt="" className="h2-like" />
-            <Image src={image.heart} alt="" className="h2-heart" />
-            <Image src={image.message} alt="" className="h2-message" />
-            <Image src={image.square} alt="" className="h2-square" />
-            <Image src={image.circle} alt="" className="moving-circle" />
-            <Image src={image.circle2} alt="" className="moving-circle-right" />
+          <div className='we-are-animation'>
+            <Image src={image.like} alt='' className='h2-like' />
+            <Image src={image.heart} alt='' className='h2-heart' />
+            <Image src={image.message} alt='' className='h2-message' />
+            <Image src={image.square} alt='' className='h2-square' />
+            <Image src={image.circle} alt='' className='moving-circle' />
+            <Image src={image.circle2} alt='' className='moving-circle-right' />
           </div>
         </div>
         <div>
-          <div className="">
-            <div className="we-are-content">
-              <div className="section-title">
-                <h2 className="pb-12 primary-heading">We Are Digital <span className="highlight">Explorers</span></h2>
+          <div className=''>
+            <div className='we-are-content'>
+              <div className='section-title'>
+                <h2 className='pb-12 primary-heading'>
+                  Your{' '}
+                  <span className='highlight'>Digital Marketing Success</span>{' '}
+                  Begins Here{' '}
+                </h2>
               </div>
 
-              <p className="app__text pb-10">
-              Everyone has gone digital with this era and so has gone the ways of promoting your work, business, and lifestyle. Digital Marketing is the boon we are talking about. WebDev Private Limited provides such convenience to you. Now that you know about digital marketing do you have the time and energy to make attractive slogans, digital posters, and attention-seeking layouts?
+              <p className='app__text pb-10'>
+                Looking for a results driven digital marketing agency in the
+                Philippines that will take your business to the new heights? You
+                are at the right place. Digital Agency Solution INC is a
+                full-service digital marketing company in the Philippines. We
+                offer a full suite of digital marketing services including Web
+                Design, Website Development, SEO services, social media
+                marketing, PPC services, content marketing, analytics services,
+                and much more.
               </p>
               <div>
-                <Link href="/about">
-                  <button className="primary-btn shadow-lg">Read More</button>
+                <Link href='/about'>
+                  <button className='primary-btn shadow-lg'>Read More</button>
                 </Link>
               </div>
             </div>
